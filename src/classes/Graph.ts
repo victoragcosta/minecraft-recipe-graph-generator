83845f065df12d878d561cd47766e4f8550e9432
Victor André Gris Costa <victorandr98@gmail.com>
2023-04-14 12:21:35 -0300

    Graph: finish basic graph implementation

--- src/classes/Graph.ts
+++ src/classes/Graph.ts
@@ -1,5 +1,10 @@
 import GraphNode, { GraphEdge } from "./GraphNode";
 
+type OutboundInboundGraphEdge = {
+  outbound: GraphEdge,
+  inbound: GraphEdge,
+}
+
 export type AddEdgeParams = {
   start: GraphNode,
   end: GraphNode,
@@ -7,9 +12,24 @@ export type AddEdgeParams = {
   data?: unknown,
 }
 
-export class NodeNotFoundError extends Error {
+export class GraphError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+export class NodeNotFoundError extends GraphError {
   constructor() {
     super("Node Not Found: the specified node was not found inside the graph.");
+  }
+}
+export class EdgeNotFoundError extends GraphError {
+  constructor() {
+    super("Edge Not Found: the specified edge was not found inside the graph.");
+  }
+}
+export class InconsistentGraphError extends GraphError {
+  constructor(message: string) {
+    super(`Inconsistent Graph Error: ${message}`);
   }
 }
 
@@ -20,41 +40,132 @@ export default class Graph {
     this._nodeList = [];
   }
 
+  /**
+   * Finds a node in the graph via reference.
+   * @param node The reference to a node that is in the graph.
+   * @returns The exact same node
+   * @throws NodeNotFoundError
+   */
+  private findNode(node: GraphNode): GraphNode {
+    const foundNode = this._nodeList.find(n => n === node);
+
+    if (!foundNode) {
+      throw new NodeNotFoundError();
+    }
+
+    return foundNode;
+  }
+  /**
+   * Finds a edge in the graph via reference.
+   * @param edge The reference to a edge that is in the graph.
+   * @returns The outbound and inbound edges in an object.
+   * @throws EdgeNotFoundError
+   * @throws InconsistentGraphError
+   */
+  private findEdge(edge: GraphEdge): OutboundInboundGraphEdge {
+    try {
+      const startNode = this.findNode(edge.startNode);
+      const endNode = this.findNode(edge.endNode);
+
+      const foundEdge = startNode.outboundEdge.find(e => e === edge);
+      const foundReverseEdge = endNode.inboundEdge.find(e => e === edge);
+
+      if (!foundEdge && !foundReverseEdge) {
+        throw new EdgeNotFoundError();
+      } else if (!foundEdge) {
+        throw new InconsistentGraphError("there's the outbound but not the inbound edge in the graph.");
+      } else if (!foundReverseEdge) {
+        throw new InconsistentGraphError("there's the inbound edge but not the outbound edge in the graph");
+      }
+
+      return {
+        outbound: foundEdge,
+        inbound: foundReverseEdge,
+      };
+    } catch (err) {
+      if (err instanceof NodeNotFoundError) {
+        throw new EdgeNotFoundError();
+      }
+      throw err;
+    }
+  }
+
   public get nodeList(): GraphNode[] {
     return this._nodeList;
   }
 
+  /**
+   * Adds a node to this instance of Graph.
+   * @param node The instantiated node to be added.
+   */
   public addNode(node: GraphNode) {
     this._nodeList.push(node);
   }
-  public removeNode(node: GraphNode) {
-    throw new Error("Method not implemented");
-  }
+  /**
+   * Removes a node and all its edges from the graph.
+   * @param node The reference to a node that exists in the graph.
+   * @returns The removed node.
+   * @throws NodeNotFoundError
+   */
+  public removeNode(node: GraphNode): GraphNode {
+    // Check to see if there is the node. Will throw an error if not.
+    const foundNode = this.findNode(node);
 
-  public addEdge(params: AddEdgeParams) {
-    throw new Error("Method not implemented");
-
-    const foundStart = this._nodeList.find(n => n === params.start);
-    const foundEnd = this._nodeList.find(n => n === params.end);
-
-    if (!foundStart || !foundEnd) {
-      throw new NodeNotFoundError();
+    // Remove all edges
+    for (const edge of foundNode.outboundEdge) {
+      edge.endNode.removeEdge(foundNode, "in");
+    }
+    for (const edge of foundNode.inboundEdge) {
+      edge.startNode.removeEdge(foundNode, "out");
     }
 
-    foundStart.addEdge({
+    // Remove the node from the graph
+    this._nodeList.splice(this._nodeList.indexOf(foundNode), 1);
+    return foundNode;
+  }
+
+  /**
+   * Adds a directed edge to this instance of Graph.
+   * @param params The parameters of the edge being added.
+   * @returns The reference to the outbound edge that has been added.
+   * @throws NodeNotFoundError
+   */
+  public addEdge(params: AddEdgeParams): GraphEdge {
+
+    // Check to see if there are the nodes. Will throw an error if not.
+    const foundStart = this.findNode(params.start);
+    const foundEnd = this.findNode(params.end);
+
+    // Add reverse reference to edge
+    foundEnd.addEdge({
+      node: foundStart,
+      direction: "in",
+      weight: params.weight,
+      data: params.data,
+    });
+    // Add edge
+    return foundStart.addEdge({
       node: foundEnd,
       direction: "out",
       weight: params.weight,
       data: params.data,
     });
-    foundEnd.addEdge({
-      node: foundStart,
-      direction: "out",
-      weight: params.weight,
-      data: params.data,
-    });
   }
-  public removeEdge(edge: GraphEdge) {
-    throw new Error("Method not implemented");
+  /**
+   * Removes an specific edge from this instance of Graph.
+   * @param edge The reference to an existing outbound edge in this graph.
+   * @returns The removed edge.
+   * @throws EdgeNotFoundError
+   * @throws InconsistentGraphError
+   */
+  public removeEdge(edge: GraphEdge): GraphEdge {
+    // Check if there's an edge
+    const { outbound, inbound } = this.findEdge(edge);
+
+    // Remove edge in both directions
+    outbound.startNode.removeEdge(outbound.endNode, "out");
+    inbound.endNode.removeEdge(inbound.startNode, "in");
+
+    return outbound;
   }
 }
